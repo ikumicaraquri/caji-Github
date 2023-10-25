7dc895295def7c35694d20b6dca7b99d528b3798
tendoikumi <tendo.ikumi.caraquri@gmail.com>
2023-10-25 10:17:57 +0900

    Xcodeを読み込む

---
+++ caji-Github/caji-Github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  caji-Github
+//
+//  Created by 天道育未 on 2023/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ caji-Github/caji-Github/caji_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  caji_GithubApp.swift
+//  caji-Github
+//
+//  Created by 天道育未 on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct caji_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
